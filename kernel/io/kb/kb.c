974cb2e965daa1c6a3c36abdb56839af953e21cd
Aaron Blakely <aaron@ephasic.org>
2012-06-20 13:02:41 -0400

    erm, saving things is nice.

--- kernel/io/kb/kb.c
+++ kernel/io/kb/kb.c
@@ -13,3 +13,46 @@
 #include <tty/color.h>
 
 state = 0;
+
+void keyboard_handler(struct regs *r)
+{
+	unsigned char scancode;
+	scancode = inportb(0x60);
+
+	if (scancode & 0x80)
+	{
+		if ((char)kbmap[scancode] == LEFT_SHIFT || (char)kbmap[scancode] == RIGHT_SHIFT)
+		{
+			state--;
+		}
+		else if (kbmap[scancode] == CAPS_LOCK)
+		{
+			if (state == 0)
+			{
+				state++;
+			} else {
+				state--;
+			}
+		}
+	}
+	else {
+		if ((char)kbmap[scancode] == LEFT_SHIFT || (char)kbmap[scancode] == RIGHT_SHIFT)
+		{
+			state++;
+		}
+		else {
+			if (state == 1)
+			{
+				put(uckbmap[scancode]);
+			}
+			else {
+				put(kbmap[scancode]);
+			}
+		}
+	}
+}
+
+void keyboard_install()
+{
+	irq_install_handler(1, keyboard_handler, "keyboard");
+}
