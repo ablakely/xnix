8933a24d133110b445ae2754adacea965fcceab6
Aaron Blakely <aaron@ephasic.org>
2013-01-25 20:26:28 -0600

    Update tools/mkcrosscompiler.sh

--- tools/mkcrosscompiler.sh
+++ tools/mkcrosscompiler.sh
@@ -57,7 +57,7 @@ echo "Done installing gcc."
 
 echo "Your environment should now have a working i586-elf cross compiler tool chain!"
 echo 'But first, you need to do this:'
-echo '	export PATH=PATH:/usr/local/cross/bin'
+echo '	export PATH=$PATH:/usr/local/cross/bin'
 echo " "
 echo "Also make sure you have a working nasm assembler."
 
